bfee9925928b6343937c54d5b745d14b12fe8202
4johnny <4johnny@users.noreply.github.com>
2015-02-22 16:57:55 -0800

    Add ORN Ops project to workspace

---
+++ ORN-Ops/ORN-Ops/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ORN-Ops
+//
+//  Created by Johnny on 2015-02-22.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ORN-Ops/ORN-OpsTests/ORN_OpsTests.m
@@ -0,0 +1,40 @@
+//
+//  ORN_OpsTests.m
+//  ORN-OpsTests
+//
+//  Created by Johnny on 2015-02-22.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ORN_OpsTests : XCTestCase
+
+@end
+
+@implementation ORN_OpsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
