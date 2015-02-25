ad8a9f8ce7356256564c0d0a43c5bad1a528100b
4johnny <4johnny@users.noreply.github.com>
2015-02-24 22:21:18 -0800

    Add data model for Rides

--- ORN-Ops/ORN-Ops/AppDelegate.h
+++ ORN-Ops/ORN-Ops/AppDelegate.h
@@ -7,11 +7,40 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+
+#
+# pragma mark - Interface
+#
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+#
+# pragma mark Properties
+#
+
 @property (strong, nonatomic) UIWindow *window;
 
+#
+# pragma mark Core Data Properties
+#
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+
+#
+# pragma mark Core Data Helpers
+#
+
+- (void)saveManagedObjectContext;
+
+#
+# pragma mark Helpers
+#
+
++ (NSURL *)applicationDocumentsDirectoryURL;
 
 @end
 
--- ORN-Ops/ORN-Ops/AppDelegate.m
+++ ORN-Ops/ORN-Ops/AppDelegate.m
@@ -8,9 +8,31 @@
 
 #import "AppDelegate.h"
 
+
+#
+# pragma mark - Interface
+#
+
+
 @interface AppDelegate ()
 
+
+#
+# pragma mark Core Data Properties
+#
+
+// NOTE: Private accessors are read-write
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+
 @end
+
+
+#
+# pragma mark - Implementation
+#
+
 
 @implementation AppDelegate
 
@@ -41,5 +63,114 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
 	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+#
+# pragma mark - Core Data Stack
+#
+
+
+#
+# pragma mark Core Data Accessors
+#
+
+
+//
+// Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.)
+//
+- (NSManagedObjectContext *)managedObjectContext {
+	
+	if (_managedObjectContext) return _managedObjectContext;
+	
+	NSPersistentStoreCoordinator *psc = self.persistentStoreCoordinator;
+	if (!psc) return nil;
+	
+	_managedObjectContext = [[NSManagedObjectContext alloc] init]; // Init call makes this thread default owner
+	_managedObjectContext.persistentStoreCoordinator = psc;
+	
+	return _managedObjectContext;
+}
+
+
+//
+// The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it.
+//
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
+	
+	if (_persistentStoreCoordinator) return _persistentStoreCoordinator;
+	
+	// Create the coordinator and store
+	_persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:self.managedObjectModel];
+	NSURL *storeURL = [[AppDelegate applicationDocumentsDirectoryURL] URLByAppendingPathComponent:@"EveryDoAgain.sqlite"];
+	NSError *error = nil;
+	if ([_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) return _persistentStoreCoordinator;
+	
+	// TODO: Replace this with code to handle the error appropriately.
+	// abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+	error = [AppDelegate persistentStoreError:error];
+	NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+	abort();
+	
+	return _persistentStoreCoordinator;
+}
+
+
+//
+// The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
+//
+- (NSManagedObjectModel *)managedObjectModel {
+	
+	if (_managedObjectModel) return _managedObjectModel;
+	
+	NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"EveryDoAgain" withExtension:@"momd"];
+	_managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+	
+	return _managedObjectModel;
+}
+
+
+#
+# pragma mark Core Data Helpers
+#
+
+
+- (void)saveManagedObjectContext {
+	
+	NSManagedObjectContext *moc = self.managedObjectContext;
+	if (!moc || !moc.hasChanges) return;
+	
+	NSError *error = nil;
+	if ([moc save:&error]) return;
+	
+	// TODO: Replace this with code to handle the error appropriately.
+	// abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+	NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+	abort();
+}
+
+
++ (NSError *)persistentStoreError:(NSError*)error {
+	
+	NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:3];
+	
+	dict[NSLocalizedDescriptionKey] = @"Failed to initialize the application's saved data";
+	dict[NSLocalizedFailureReasonErrorKey] = @"There was an error creating or loading the application's saved data.";
+	dict[NSUnderlyingErrorKey] = error;
+	
+	error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
+	
+	return error;
+}
+
+
+#
+# pragma mark - Helpers
+#
+
+
++ (NSURL *)applicationDocumentsDirectoryURL {
+	
+	return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
+
 
 @end
