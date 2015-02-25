e9c8663bc2949cb843d31fe8339a92f917555ca1
4johnny <4johnny@users.noreply.github.com>
2015-02-24 22:51:14 -0800

    Add data model auto-generated classes for Rides

---
+++ ORN-Ops/ORN-Ops/Ride.h
@@ -0,0 +1,33 @@
+//
+//  Ride.h
+//  ORN-Ops
+//
+//  Created by Johnny on 2015-02-24.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Ride : NSManagedObject
+
+@property (nonatomic, retain) NSDate * dateTimeEnd;
+@property (nonatomic, retain) NSDate * dateTimeStart;
+@property (nonatomic, retain) NSString * locationEndAddress;
+@property (nonatomic, retain) NSNumber * locationEndLatitude;
+@property (nonatomic, retain) NSNumber * locationEndLongitude;
+@property (nonatomic, retain) NSString * locationStartAddress;
+@property (nonatomic, retain) NSNumber * locationStartLatitude;
+@property (nonatomic, retain) NSNumber * locationStartLongitude;
+@property (nonatomic, retain) NSNumber * passengerCount;
+@property (nonatomic, retain) NSString * passengerNameFirst;
+@property (nonatomic, retain) NSString * passengerNameLast;
+@property (nonatomic, retain) NSString * passengerPhoneNumber;
+@property (nonatomic, retain) NSString * transferFrom;
+@property (nonatomic, retain) NSString * transferTo;
+@property (nonatomic, retain) NSString * vehicleDescription;
+@property (nonatomic, retain) NSNumber * vehicleSeatbeltCount;
+@property (nonatomic, retain) NSString * vehicleTransmission;
+
+@end
---
+++ ORN-Ops/ORN-Ops/Ride.m
@@ -0,0 +1,32 @@
+//
+//  Ride.m
+//  ORN-Ops
+//
+//  Created by Johnny on 2015-02-24.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import "Ride.h"
+
+
+@implementation Ride
+
+@dynamic dateTimeEnd;
+@dynamic dateTimeStart;
+@dynamic locationEndAddress;
+@dynamic locationEndLatitude;
+@dynamic locationEndLongitude;
+@dynamic locationStartAddress;
+@dynamic locationStartLatitude;
+@dynamic locationStartLongitude;
+@dynamic passengerCount;
+@dynamic passengerNameFirst;
+@dynamic passengerNameLast;
+@dynamic passengerPhoneNumber;
+@dynamic transferFrom;
+@dynamic transferTo;
+@dynamic vehicleDescription;
+@dynamic vehicleSeatbeltCount;
+@dynamic vehicleTransmission;
+
+@end
